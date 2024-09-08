7cf10359b0b7fc4bd23748c65e3a340c25e7d8fb
nhn019 <nhn019@uowmail.edu.au>
2024-09-08 23:30:48 +1000

    complete 1.9 - step 5

--- part1/uniCafe/src/App.jsx
+++ part1/uniCafe/src/App.jsx
@@ -15,21 +15,14 @@ const Display = ({text, count}) => {
   </p>)
 }
 
-const Statistics = ({good, neutral, bad, all, average, positive, setAverage, setPositive}) => {
-  
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good/all)*100+"%";
 
-  // calculate average score
-  const updatedAverage = (good - bad)/all;
-  setAverage(updatedAverage);
-
-  //calculate positive score
-  const updatedPositive = (good/all)*100 + "%";
-  setPositive(updatedPositive);
-
-  //rendering statistics
   if (all === 0) {
     return (<p>No feedback given</p>)
-  } else {
+  }
   return (
     <div>
       <Display text="good" count={good}/>
@@ -42,37 +35,27 @@ const Statistics = ({good, neutral, bad, all, average, positive, setAverage, set
     </div>
   )
 }
-}
+// }
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+
 
   //count clicks
   const onClickGood = () => {
     const updatedGood = good+1
     setGood(updatedGood);
-    const updatedAll = updatedGood+neutral+bad;
-    setAll(updatedAll);
   }
   const onClickNeutral = () => {
     const updatedNeutral = neutral+1;
     setNeutral(updatedNeutral);
-    const updatedAll = good+updatedNeutral+bad;
-    setAll(updatedAll);
-    
   }
   const onClickBad = () => {
     const updatedBad = bad+1;
     setBad(updatedBad);
-    const updatedAll = good+neutral+updatedBad;
-    setAll(updatedAll);
-    
   }
 
   return (
@@ -88,11 +71,6 @@ const App = () => {
         good={good} 
         neutral={neutral} 
         bad={bad} 
-        all={all} 
-        average={average}
-        positive={positive}
-        setAverage={(updatedAverage)=>setAverage(updatedAverage)}
-        setPositive={(updatedPositive)=>setPositive(updatedPositive)}
       />
 
       
